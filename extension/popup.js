ae2daa462a698102f549e18b7756f6e9f15e0a92
Folyd <lyshuhow@gmail.com>
2019-01-31 21:52:24 +0800

    Support configure offline doc path

--- extension/popup.js
+++ extension/popup.js
@@ -5,8 +5,45 @@ document.addEventListener('DOMContentLoaded', function() {
         openTypeSelect.selected = true;
     }
     openTypeSelect.onchange = onOpenTypeChange;
+
+    var offlineModeCheckbox = document.querySelector('input[type="checkbox"]');
+    offlineModeCheckbox.onchange = onOfflineModeChange;
+    // JSON parse 'true' or 'false' string to boolean value
+    var checkedState = nullOrDefault(JSON.parse(localStorage.getItem('offline-mode')), false);
+    offlineModeCheckbox.checked = checkedState;
+    toggleOfflinePathEnableState(checkedState);
+
+    var offlineDocPath = document.querySelector('.offline-doc-path');
+    offlineDocPath.onchange = onOfflinePathChange;
+    offlineDocPath.value = localStorage.getItem('offline-path');
 }, false);
 
 function onOpenTypeChange(event) {
     localStorage.setItem("open-type", event.target.value);
+}
+
+function onOfflineModeChange(event) {
+    var enable = event.target.checked;
+    localStorage.setItem('offline-mode', enable);
+    toggleOfflinePathEnableState(enable);
+}
+
+function toggleOfflinePathEnableState(enable) {
+    var offlineDocPath = document.querySelector('.offline-doc-path');
+    if (enable) {
+        offlineDocPath.classList.remove('disable');
+        offlineDocPath.classList.add('enable');
+    } else {
+        offlineDocPath.classList.remove('enable');
+        offlineDocPath.classList.add('disable');
+    }
+}
+
+function onOfflinePathChange(event) {
+    var offlineDocPath = document.querySelector('.offline-doc-path');
+    localStorage.setItem("offline-path", offlineDocPath.value);
+}
+
+function nullOrDefault(value, default_value) {
+    return value === null ? default_value : value;
 }
