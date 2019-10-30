d12e2300b050a76991c1caa1540b8aa7a78868b2
Folyd <lyshuhow@gmail.com>
2019-10-30 19:29:44 +0800

    Fix offline mode search bug

--- extension/popup.js
+++ extension/popup.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var openTypeSelect = document.querySelector('select[name="open-type"]');
+    const openTypeSelect = document.querySelector('select[name="open-type"]');
     if (localStorage.getItem("open-type")) {
         openTypeSelect.value = localStorage.getItem("open-type");
         openTypeSelect.selected = true;
     }
     openTypeSelect.onchange = onOpenTypeChange;
 
-    var offlineModeCheckbox = document.querySelector('input[type="checkbox"]');
+    const offlineModeCheckbox = document.querySelector('input[type="checkbox"]');
     offlineModeCheckbox.onchange = onOfflineModeChange;
     // JSON parse 'true' or 'false' string to boolean value
-    var checkedState = nullOrDefault(JSON.parse(localStorage.getItem('offline-mode')), false);
+    const checkedState = nullOrDefault(JSON.parse(localStorage.getItem('offline-mode')), false);
     offlineModeCheckbox.checked = checkedState;
     toggleOfflinePathEnableState(checkedState);
 
-    var offlineDocPath = document.querySelector('.offline-doc-path');
+    const offlineDocPath = document.querySelector('.offline-doc-path');
     offlineDocPath.onchange = onOfflinePathChange;
     offlineDocPath.value = localStorage.getItem('offline-path');
 }, false);
@@ -23,13 +23,13 @@ function onOpenTypeChange(event) {
 }
 
 function onOfflineModeChange(event) {
-    var enable = event.target.checked;
+    const enable = event.target.checked;
     localStorage.setItem('offline-mode', enable);
     toggleOfflinePathEnableState(enable);
 }
 
 function toggleOfflinePathEnableState(enable) {
-    var offlineDocPath = document.querySelector('.offline-doc-path');
+    const offlineDocPath = document.querySelector('.offline-doc-path');
     if (enable) {
         offlineDocPath.classList.remove('disable');
         offlineDocPath.classList.add('enable');
@@ -40,7 +40,7 @@ function toggleOfflinePathEnableState(enable) {
 }
 
 function onOfflinePathChange(event) {
-    var offlineDocPath = document.querySelector('.offline-doc-path');
+    const offlineDocPath = document.querySelector('.offline-doc-path');
     localStorage.setItem("offline-path", offlineDocPath.value);
 }
 
