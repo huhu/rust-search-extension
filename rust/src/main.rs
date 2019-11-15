e95b31827a517fe215766f08423c5cbd69f0a397
Folyd <lyshuhow@gmail.com>
2019-11-15 17:44:42 +0800

    Generate basic crate-ids.js file

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,14 @@
+use std::collections::HashSet;
+use std::fs;
+use std::path::Path;
+
+use futures::future::join_all;
 use serde_derive::{Deserialize, Serialize};
+use serde_json;
 use tokio;
 
-static API: &'static str = "https://crates.io/api/v1/crates?page={}&per_page=100&sort=downloads";
+const API: &'static str = "https://crates.io/api/v1/crates?page={}&per_page=100&sort=downloads";
+const CRATES_INDEX_PATH: &'static str = "../extension/crates-index.js";
 
 #[derive(Serialize, Deserialize, Debug)]
 struct CrateApiResponse {
@@ -20,12 +27,35 @@ struct Crate {
     max_version: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp: CrateApiResponse = reqwest::get(&API.replace("{}", "1"))
-        .await?
-        .json()
-        .await?;
-    println!("{:#?}", resp);
+async fn fetch_crates(page: u32) -> Result<Vec<Crate>, Box<dyn std::error::Error>> {
+    let resp: CrateApiResponse = reqwest::get(&API.replace("{}", &page.to_string())).await?
+        .json().await?;
+    Ok(resp.crates)
+}
+
+async fn generate_javascript_crates_index(crates: Vec<Crate>) -> std::io::Result<()> {
+    let mut crate_ids = HashSet::new();
+    for krate in crates {
+        crate_ids.insert(krate.id);
+    }
+
+    let mut contents = String::from("");
+    let path = Path::new(CRATES_INDEX_PATH);
+    contents.push_str(&format!("var crateIds={};", serde_json::to_string(&crate_ids).unwrap()));
+    fs::write(path, &contents)?;
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let mut futures = vec![];
+    for page in 1..10 {
+        futures.push(fetch_crates(page));
+    }
+    let crates: Vec<Crate> = join_all(futures).await
+        .into_iter().flat_map(|item| item.unwrap()).collect();
+    match generate_javascript_crates_index(crates).await {
+        Ok(_) => println!("\nGenerate javascript crates index successful!"),
+        Err(error) => println!("{:?}", error),
+    }
 }
