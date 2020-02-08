7d8a6efb85abd32e741695deb01ba6bcfca01a70
Folyd <lyshuhow@gmail.com>
2020-02-08 12:25:56 +0800

    Increase timeout to 15 seconds, add retry fetch crates strategy

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::path::Path;
 use std::sync::RwLock;
 
-use futures::future::join_all;
+use futures::future::try_join_all;
 use reqwest;
 use serde::{Deserialize, Deserializer};
 use serde_derive::Deserialize;
@@ -55,7 +55,7 @@ async fn fetch_crates(page: u32) -> Result<Vec<Crate>, Box<dyn std::error::Error
     // Keep 1 second sleep interval to comply crates.io crawler policy.
     tokio::time::delay_for(Duration::from_secs((page - 1) as u64)).await;
     let client = reqwest::Client::builder()
-        .timeout(std::time::Duration::from_secs(10))
+        .timeout(std::time::Duration::from_secs(15))
         .user_agent(USER_AGENT)
         .build()?;
     let resp: CrateApiResponse = client
@@ -92,6 +92,15 @@ async fn generate_javascript_crates_index(
     Ok(contents)
 }
 
+async fn try_fetch_all_crates() -> Result<Vec<Vec<Crate>>, Box<dyn std::error::Error>> {
+    let mut futures = vec![];
+    for page in 1..=150 {
+        futures.push(fetch_crates(page));
+    }
+
+    try_join_all(futures).await
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -100,27 +109,29 @@ async fn main() -> std::io::Result<()> {
         None => CRATES_INDEX_PATH,
     };
     let path = Path::new(path_name);
-
-    let mut futures = vec![];
-    for page in 1..=150 {
-        futures.push(fetch_crates(page));
+    let mut retry = 0;
+    loop {
+        match try_fetch_all_crates().await {
+            Ok(result) => {
+                let crates = result.into_iter().flatten().collect();
+                // Extract frequency word mapping
+                let minifier = Minifier::new(&CRATE_DESCRIPTIONS.read().unwrap());
+                let mapping = minifier.get_mapping();
+                let mut contents =
+                    format!("var mapping={};", serde_json::to_string(&mapping).unwrap());
+                contents.push_str(&generate_javascript_crates_index(crates, &minifier).await?);
+                fs::write(path, &contents)?;
+                println!("\nGenerate javascript crates index successful!");
+            }
+            Err(error) => {
+                retry += 1;
+                println!("{} Error: {:?}", retry, error);
+                if retry > 5 {
+                    println!("Failed: exceed the max retry time...");
+                    break;
+                }
+            }
+        }
     }
-    let crates: Vec<Crate> = join_all(futures)
-        .await
-        .into_iter()
-        .flat_map(|item| {
-            item.unwrap_or_else(|error| {
-                println!("{:?}", error);
-                vec![]
-            })
-        })
-        .collect();
-    // Extract frequency word mapping
-    let minifier = Minifier::new(&CRATE_DESCRIPTIONS.read().unwrap());
-    let mapping = minifier.get_mapping();
-    let mut contents = format!("var mapping={};", serde_json::to_string(&mapping).unwrap());
-    contents.push_str(&generate_javascript_crates_index(crates, &minifier).await?);
-    fs::write(path, &contents)?;
-    println!("\nGenerate javascript crates index successful!");
     Ok(())
 }
