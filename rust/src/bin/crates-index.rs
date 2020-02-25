72bb099911f198268093fe1debe36af0ce4dfc96
Folyd <lyshuhow@gmail.com>
2020-02-25 15:15:52 +0800

    Check char boundary to prevent panic

--- rust/src/bin/crates-index.rs
+++ rust/src/bin/crates-index.rs
@@ -39,31 +39,6 @@ struct Crate {
     max_version: Option<String>,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug)]
-enum TryTruncateError {
-    #[non_exhaustive]
-    NotInCharBoundary(String),
-}
-
-trait TryTruncate {
-    fn try_truncate(&mut self, new_len: usize) -> Result<(), TryTruncateError>;
-}
-
-impl TryTruncate for String {
-    fn try_truncate(&mut self, new_len: usize) -> Result<(), TryTruncateError> {
-        let mut i = new_len;
-        while !self.is_char_boundary(i) && i > 0 {
-            i -= 1;
-        }
-        if i > 0 {
-            self.truncate(i);
-            Ok(())
-        } else {
-            Err(TryTruncateError::NotInCharBoundary(self.clone()))
-        }
-    }
-}
-
 #[inline]
 fn deserialize_crate_id<'de, D>(d: D) -> Result<String, D::Error>
 where
@@ -89,8 +64,9 @@ where
 {
     Ok(Option::<String>::deserialize(d)?.map(|mut value| {
         value = value.trim().to_string();
-        if let Err(error) = value.try_truncate(100) {
-            println!("{:?}", error)
+        // Check char boundary to prevent panic
+        if value.is_char_boundary(100) {
+            value.truncate(100);
         }
         SPLITTED_WORDS.write().unwrap().push(value.clone());
         value
