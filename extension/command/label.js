156592d0f0f64f31b11dc05466d215935be46eed
Folyd <lyshuhow@gmail.com>
2020-04-15 19:32:36 +0800

    Fix label command filter bug

--- extension/command/label.js
+++ extension/command/label.js
@@ -7,28 +7,29 @@ class LabelCommand extends Command {
     }
 
     onExecute(arg) {
-        let results = [];
-        for (let label of this.labels) {
-            let index = label.name.toLowerCase().indexOf(arg);
-            if (index > -1) {
-                label["matchIndex"] = index;
-                results.push(label);
-            }
-        }
-
-        return results.sort((a, b) => {
-            if (a.matchIndex === b.matchIndex) {
-                return a.name.length - b.name.length;
-            }
-            return a.matchIndex - b.matchIndex;
-        })
-            .map(label => {
-                return {
-                    content: `https://github.com/rust-lang/rust/labels/${label.name}`,
-                    description: `${c.match(label.name)} - ${c.dim(c.escape(label.description))}`
+        let results = this.labels;
+        if (arg) {
+            results = [];
+            for (let label of this.labels) {
+                let index = label.name.toLowerCase().indexOf(arg);
+                if (index > -1) {
+                    label["matchIndex"] = index;
+                    results.push(label);
                 }
+            }
+
+            results = results.sort((a, b) => {
+                if (a.matchIndex === b.matchIndex) {
+                    return a.name.length - b.name.length;
+                }
+                return a.matchIndex - b.matchIndex;
             });
+        }
+        return results.map(label => {
+            return {
+                content: `https://github.com/rust-lang/rust/labels/${label.name}`,
+                description: `${c.match(label.name)} - ${c.dim(c.escape(label.description))}`
+            }
+        });
     }
-
-
 }
