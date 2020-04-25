6afca28323b0372b827c7b31c769e492c6e8e807
Folyd <lyshuhow@gmail.com>
2020-04-25 16:20:25 +0800

    Distinct browser type strictly

--- extension/compat.js
+++ extension/compat.js
@@ -1,18 +1,28 @@
 function Compat() {
-    this.isChrome = navigator.userAgent.toLowerCase().indexOf("chrome") !== -1;
-
     // Firefox doesn't support tags in search suggestion.
-    this.tagged = this.isChrome ?
+    this.tagged = this.browserType() !== "firefox" ?
         (tag, str) => `<${tag}>${str}</${tag}>` :
         (_, str) => str;
     this.match = (str) => this.tagged("match", str);
     this.dim = (str) => this.tagged("dim", str);
 }
 
+Compat.prototype.browserType = function() {
+    let userAgent = navigator.userAgent.toLowerCase();
+    if (userAgent.indexOf("firefox") !== -1) return "firefox";
+    if (userAgent.indexOf("edge") !== -1) return "edge";
+    if (userAgent.indexOf("chrome") !== -1) return "chrome";
+    return "unknown";
+};
+
+Compat.prototype.omniboxPageSize = function() {
+    return {"firefox": 8, "edge": 7, "chrome": 8, "unknown": 6}[this.browserType()];
+};
+
 // Escape the five predefined entities to display them as text.
 Compat.prototype.escape = function(str) {
     str = str || "";
-    return this.isChrome ? str
+    return this.browserType() !== "firefox" ? str
             .replace(/&/g, "&amp;")
             .replace(/</g, "&lt;")
             .replace(/>/g, "&gt;")
