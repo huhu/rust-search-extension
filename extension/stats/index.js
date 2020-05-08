4c4ae159148bf537ce1e90f4042c2952ccf2e9b5
shwin0901 <401815708@qq.com>
2020-05-08 22:56:47 +0800

    Add histogram charts

--- extension/stats/index.js
+++ extension/stats/index.js
@@ -1,21 +1,33 @@
-let now = moment().endOf('day').toDate();
-let yearAgo = moment().startOf('day').subtract(1, 'year').toDate();
+let calendarData = [];
+let weeks = { "Sun": 0, "Mon": 0, "Tue": 0, "Wed": 0, "Thu": 0, "Fri": 0, "Sat": 0 };
+let dates = num(1, 31);
+let hours = num(0, 23);
 
+let w = Object.keys(weeks);
+let d = Object.keys(dates);
+let h = Object.keys(hours);
 
-let chartData = [];
-let history = JSON.parse(localStorage.getItem("history"));
-history.forEach(element => {
-    chartData.push({
-        date: new Date(element.time),
+const history = JSON.parse(localStorage.getItem("history"));
+history.forEach(({ time }) => {
+    let date = new Date(time);
+    calendarData.push({
+        date,
         count: 1
-    })
+    });
+    weeks[w[date.getDay()]] += 1;
+    dates[d[date.getDate() - 1]] += 1;
+    hours[h[date.getHours()]] += 1;
+});
+
+let [weeksData, datesData, hoursData] = [weeks, dates, hours].map(data => {
+    return Object.entries(data).map(([key, value]) => { return { name: key, value } })
 });
 
 let heatmap = calendarHeatmap()
-    .data(chartData)
+    .data(calendarData)
     .selector('.chart-heatmap')
     .tooltipEnabled(true)
-    .colorRange(['#f4f7f7','#F9BB2D'])
+    .colorRange(['#f4f7f7', '#F9BB2D'])
     .tooltipUnit([
         { min: 0, unit: 'searching' },
         { min: 1, max: 1, unit: 'searching' },
@@ -25,4 +37,40 @@ let heatmap = calendarHeatmap()
     .onClick(function (data) {
         console.log('data', data);
     });
-heatmap(); 
+heatmap();
+
+
+function num(start, end) {
+    let obj = {};
+    for (let i = start; i <= end; i++) {
+        obj[i] = 0;
+    }
+    return obj;
+};
+
+histogram({
+    selector: ".chart-histogram-week",
+    data: weeksData,
+    width: 550,
+    height: 320,
+    color: "#F9BB2D",
+    margin: { top: 30, right: 0, bottom: 40, left: 40 },
+});
+
+histogram({
+    selector: ".chart-histogram-date",
+    data: datesData,
+    width: 550,
+    height: 320,
+    color: "#F9BB2D",
+    margin: { top: 30, right: 0, bottom: 40, left: 40 },
+});
+
+histogram({
+    selector: ".chart-histogram-hour",
+    data: hoursData,
+    width: 550,
+    height: 320,
+    color: "#F9BB2D",
+    margin: { top: 30, right: 0, bottom: 40, left: 40 },
+});
