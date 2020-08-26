494fc14f9c44ae37543e5ed6f57bbb8b3a02b20a
Folyd <lyshuhow@gmail.com>
2020-08-26 16:32:29 +0800

    Change :help command content to prefix

--- extension/command/help.js
+++ extension/command/help.js
@@ -4,19 +4,19 @@ class HelpCommand extends Command {
     }
 
     onExecute() {
-        const value = ([
-            `Prefix ${c.match(":")} to execute command (:book, :yet, :stable, etc)`,
-            `Prefix ${c.match("/")} to search nightly rust docs`,
-            `Prefix ${c.match("!")} to search crates, prefix ${c.match("!!")} to search crates's docs, prefix ${c.match("!!!")} can redirect to the repository`,
-            `Prefix ${c.match("~")} to search external crate's docs`,
-            `Prefix ${c.match("@crate")} (${c.dim("e.g. @tokio")}) to search that crate's doc exclusively`,
-            `Prefix ${c.match("#")} to search builtin attributes`,
-            `Prefix ${c.match("%")} to search Rust official book chapters`,
-            `Prefix ${c.match(">")} to search Rust clippy lints`,
-            `[WIP] Prefix ${c.match("?")} to search Rust tracking issues`,
-        ]);
-        return value.map((description, index) => {
-            return {content: `${index + 1}`, description};
+        const value = {
+            ":": `Prefix ${c.match(":")} to execute command (:book, :yet, :stable, etc)`,
+            "/": `Prefix ${c.match("/")} to search nightly rust docs`,
+            "!": `Prefix ${c.match("!")} to search crates, prefix ${c.match("!!")} to search crates's docs, prefix ${c.match("!!!")} can redirect to the repository`,
+            "~": `Prefix ${c.match("~")} to search external crate's docs`,
+            "@": `Prefix ${c.match("@crate")} (${c.dim("e.g. @tokio")}) to search that crate's doc exclusively`,
+            "#": `Prefix ${c.match("#")} to search builtin attributes`,
+            "%": `Prefix ${c.match("%")} to search Rust official book chapters`,
+            ">": `Prefix ${c.match(">")} to search Rust clippy lints`,
+            "?": `[WIP] Prefix ${c.match("?")} to search Rust tracking issues`,
+        };
+        return Object.entries(value).map(([key, description], index) => {
+            return {content: key, description};
         });
     }
 }
