804890f8c6b80319fb0bea576aca6ac42c0a2d24
Folyd <lyshuhow@gmail.com>
2020-09-09 18:30:33 +0800

    Check version between localStorage and today to ensure update search index once a day

--- extension/script/doc-rust-lang-org.js
+++ extension/script/doc-rust-lang-org.js
@@ -1,6 +1,13 @@
 let target = location.pathname.includes("/nightly/") ? "nightly" : "stable";
 
 document.addEventListener("DOMContentLoaded", () => {
+    let version = localStorage.getItem(`rust-search-extension:${target}`);
+    let now = new Date();
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0)
+    if (version && today <= Date.parse(version)) {
+        // Check version between localStorage and today to ensure update search index once a day.
+        return;
+    }
     injectScripts(["script/add-std-search-index.js"]);
 });
 
@@ -10,7 +17,10 @@ window.addEventListener("message", function (event) {
         event.data.direction === "rust-search-extension:std") {
         chrome.runtime.sendMessage({action: `${target}:add`, ...event.data.message},
             (response) => {
-                console.log(response);
+                let now = new Date();
+                let version = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+                localStorage.setItem(`rust-search-extension:${target}`, version);
+                console.log(version);
             }
         );
     }
