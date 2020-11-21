36e2170ea983b35b3e354618248bd021a07880c7
Folyd <lyshuhow@gmail.com>
2020-11-21 18:48:40 +0800

    Refractor search statistics page (finer granularity for search metrics)

--- extension/stats/index.js
+++ extension/stats/index.js
@@ -1,10 +1,11 @@
 const history = JSON.parse(localStorage.getItem("history")) || [];
 let chartColor = "rgba(249, 188, 45, 0.5)";
-let weeks = { "Sun": 0, "Mon": 0, "Tue": 0, "Wed": 0, "Thu": 0, "Fri": 0, "Sat": 0 };
+let weeks = {"Sun": 0, "Mon": 0, "Tue": 0, "Wed": 0, "Thu": 0, "Fri": 0, "Sat": 0};
 let dates = makeNumericKeyObject(1, 31);
 let hours = makeNumericKeyObject(0, 23);
+
 function makeNumericKeyObject(start, end, initial = 0) {
-    return Array.from({ length: end + 1 - start }).fill(initial)
+    return Array.from({length: end + 1 - start}).fill(initial)
         .reduce((obj, current, index) => {
             obj[start + index] = current;
             return obj;
@@ -13,46 +14,55 @@ function makeNumericKeyObject(start, end, initial = 0) {
 
 let stats = [
     {
-        name: "Std docs",
+        name: "stable",
         pattern: null,
         value: 0,
         color: "#FEC744",
-        description: "Std docs count the non-prefix std docs searches."
+        description: "Std stable docs searches."
     },
     {
-        name: "External docs",
-        pattern: /^[~@].*/i,
-        value: 0,
-        color: "#ff00af",
-        description: `External docs count any ~ or @ prefix searches.`
-    },
-    {
-        name: "Crates",
-        pattern: /^!.*/i,
-        value: 0,
-        color: "#3D6739",
-        description: "Crates count any ! prefix searches, including !, !! and !!!."
-    },
-    {
-        name: "Attributes",
-        pattern: /^#.*/i,
+        name: "nightly",
+        pattern: /^\/.*/i,
         value: 0,
         color: "#030303",
-        description: "Attributes count any # prefix searches."
+        description: "Std nightly docs searches."
     },
     {
-        name: "Error codes",
-        pattern: /`?e\d{2,4}`?$/i,
+        name: "docs.rs",
+        pattern: /^[~@].*/i,
+        value: 0,
+        color: "#dd6b33",
+        description: "Docs.rs docs search.",
+    },
+    {
+        name: "crates",
+        // Remove pattern for crates search for history data compatibility,
+        // since we switched the prefix priority between crates and docs.rs.
+        pattern: null,
+        value: 0,
+        color: "#3D6739",
+        description: "crates.io or lib.rs searches."
+    },
+    {
+        name: "attributes",
+        pattern: /^#.*/i,
+        value: 0,
+        color: "#9e78c6",
+        description: "Built-in attributes searches."
+    },
+    {
+        name: "error codes",
+        pattern: /^`?e\d{2,4}`?$/i,
         value: 0,
         color: "#f50707",
-        description: "Error codes count any error code searches. (regex: e\\d{2,4})"
+        description: "Compile error index searches."
     },
     {
-        name: "Others",
+        name: "others",
         pattern: /^[>%?].*/i,
         value: 0,
         color: "#ededed",
-        description: "Others count any Clippy lint (>), book (%), and caniuse/rfc (?) prefix searches."
+        description: "Others including any Clippy lint (>), book (%), and caniuse/rfc (?) searches."
     },
 ];
 let calendarData = [];
@@ -62,7 +72,7 @@ let w = Object.keys(weeks);
 let d = Object.keys(dates);
 let h = Object.keys(hours);
 
-history.forEach(({ query, content, description, time }) => {
+history.forEach(({query, content, description, time}) => {
     let date = new Date(time);
     calendarData.push({
         date,
@@ -77,14 +87,17 @@ history.forEach(({ query, content, description, time }) => {
         stat.value += 1;
     } else {
         // Classify the default search cases
-        if (["https://crates.io", "https://lib.rs"].some(prefix => content.startsWith(prefix))) {
-            // Crates
+        if (content.startsWith("https://docs.rs")) {
+            // Crate docs
             stats[2].value += 1;
+        } else if (["https://crates.io", "https://lib.rs"].some(prefix => content.startsWith(prefix))) {
+            // Crates
+            stats[3].value += 1;
         } else if (description.startsWith("Attribute")) {
             // Attribute
-            stats[3].value += 1;
+            stats[4].value += 1;
         } else {
-            // Std docs (stable or nightly)
+            // Std docs (stable)
             stats[0].value += 1;
         }
     }
@@ -122,7 +135,9 @@ history.forEach(({ query, content, description, time }) => {
 });
 
 let [weeksData, datesData, hoursData] = [weeks, dates, hours].map(data => {
-    return Object.entries(data).map(([key, value]) => { return { name: key, value } })
+    return Object.entries(data).map(([key, value]) => {
+        return {name: key, value}
+    })
 });
 
 let heatmap = calendarHeatmap()
@@ -130,16 +145,16 @@ let heatmap = calendarHeatmap()
     .selector('.chart-heatmap')
     .tooltipEnabled(true)
     .colorRange([
-        { min: 0, color: '#f4f7f7' },
-        { min: 1, max: 2, color: '#ffdd2b' },
-        { min: 3, max: 6, color: '#f6a405' },
-        { min: 7, max: 11, color: '#f56b04' },
-        { min: 12, max: 'Infinity', color: '#f40703' }
+        {min: 0, color: '#f4f7f7'},
+        {min: 1, max: 2, color: '#ffdd2b'},
+        {min: 3, max: 6, color: '#f6a405'},
+        {min: 7, max: 11, color: '#f56b04'},
+        {min: 12, max: 'Infinity', color: '#f40703'}
     ])
     .tooltipUnit([
-        { min: 0, unit: 'search' },
-        { min: 1, max: 1, unit: 'searches' },
-        { min: 2, max: 'Infinity', unit: 'searches' }
+        {min: 0, unit: 'search'},
+        {min: 1, max: 1, unit: 'searches'},
+        {min: 2, max: 'Infinity', unit: 'searches'}
     ])
     .legendEnabled(true)
     .onClick(function (data) {
@@ -151,7 +166,7 @@ let histogramConfig = {
     width: 460,
     height: 240,
     color: chartColor,
-    margin: { top: 30, right: 0, bottom: 40, left: 40 }
+    margin: {top: 30, right: 0, bottom: 40, left: 40}
 };
 histogram({
     selector: ".chart-histogram-week",
@@ -175,6 +190,7 @@ let searchTimes = document.querySelector(".search-time");
 let frequency = searchTimes.querySelectorAll("b");
 frequency[0].textContent = `${history.length}`;
 frequency[1].textContent = calculateSavedTime(history.length);
+
 function calculateSavedTime(times) {
     if (times * 5 > 3600) {
         return `${Math.round(times * 5 / 3600)} hours.`;
@@ -190,10 +206,10 @@ let searchStatsText = document.querySelector(".search-stats-text");
 let ol = searchStatsText.querySelector("ol");
 stats.sort((a, b) => {
     // Others always the last
-    if (a.name === "Others" || b.name === "Others") return 0;
+    if (a.name.toLowerCase() === "others" || b.name.toLowerCase() === "others") return 0;
     return b.value - a.value;
 });
-stats.forEach(({ name, color, value, description }) => {
+stats.forEach(({name, color, value, description}) => {
     let li = document.createElement("li");
     li.innerHTML = `<div aria-label="${description}" data-balloon-pos="up" data-balloon-length="large"
                         style="text-align: center" class="tooltip-color">
@@ -217,7 +233,7 @@ topCratesData = Object.entries(topCratesData).sort((a, b) => b[1] - a[1]).map(([
 });
 topCratesData.splice(15);
 barChart({
-    margin: ({ top: 30, right: 0, bottom: 10, left: 30 }),
+    margin: ({top: 30, right: 0, bottom: 10, left: 30}),
     // Calculate height dynamically to keep the bar with consistence width regardless of the topCratesData length.
     height: 800 / 15 * topCratesData.length + 40,
     barHeight: 25,
