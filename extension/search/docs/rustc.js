a3b7df89e75141cc49babdb05ba99389e9a499e5
Folyd <lyshuhow@gmail.com>
2021-01-17 11:24:08 +0800

    Fix rustc search url

--- extension/search/docs/rustc.js
+++ extension/search/docs/rustc.js
@@ -1,10 +1,18 @@
 class RustcSearch extends DocSearch {
     constructor(searchIndex, version) {
-        super("*", searchIndex);
+        super("rustc", searchIndex);
         this.version = version;
     }
 
     get rootPath() {
         return "https://doc.rust-lang.org/nightly/nightly-rustc/";
+    }
+
+    getSearchUrl(keyword) {
+        let url = `${this.rootPath}index.html`;
+        if (keyword) {
+            url += `?search=${encodeURIComponent(keyword)}`;
+        }
+        return url;
     }
 }
