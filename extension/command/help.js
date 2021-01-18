520e48c06f1189aa6184537889c4f47fb04b96ae
Folyd <lyshuhow@gmail.com>
2021-01-18 22:09:49 +0800

    Update help command message for rustc docs search

--- extension/command/help.js
+++ extension/command/help.js
@@ -6,7 +6,7 @@ class HelpCommand extends Command {
     onExecute() {
         const value = {
             ":": `Prefix ${c.match(":")} to execute command (:cargo, :book, :yet, :stable, etc)`,
-            "/": `Prefix ${c.match("/")} to search nightly rust docs`,
+            "/": `Prefix ${c.match("/")} to search nightly rust docs, prefix ${c.match("//")} to search nightly rustc docs`,
             "!": `Prefix ${c.match("!")} to search docs.rs, prefix ${c.match("!!")} to search crates.io, prefix ${c.match("!!!")} can redirect to the repository`,
             "~": `Prefix ${c.match("~")} to search external crate's docs`,
             "@": `Prefix ${c.match("@crate")} (${c.dim("e.g. @tokio")}) to search that crate's doc exclusively`,
