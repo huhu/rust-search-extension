0230678dc8d3da5a9cb1b98aab4595fe65a43a56
Folyd <lyshuhow@gmail.com>
2021-01-28 18:30:55 +0800

    Add validation when auto index std search-index. Fix #99

--- extension/script/add-std-search-index.js
+++ extension/script/add-std-search-index.js
@@ -1,17 +1,21 @@
 (function () {
-    // Remove needless crate's search index, such as core, alloc, etc
+    const STD_CRATES = ['std', 'test', 'proc_macro'];
+
+    // Remove unnecessary std crate's search index, such as core, alloc, etc
     function cleanSearchIndex() {
         let searchIndex = {};
-        searchIndex['std'] = window.searchIndex['std'];
-        searchIndex['test'] = window.searchIndex['test'];
-        searchIndex['proc_macro'] = window.searchIndex['proc_macro'];
+        STD_CRATES.forEach(crate => {
+            searchIndex[crate] = window.searchIndex[crate];
+        });
         return searchIndex;
     }
 
-    window.postMessage({
-        direction: `rust-search-extension:std`,
-        message: {
-            searchIndex: cleanSearchIndex(window.searchIndex),
-        },
-    }, "*");
+    if (window.searchIndex && STD_CRATES.every(crate => crate in window.searchIndex)) {
+        window.postMessage({
+            direction: `rust-search-extension:std`,
+            message: {
+                searchIndex: cleanSearchIndex(window.searchIndex),
+            },
+        }, "*");
+    }
 })();
