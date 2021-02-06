b16f59f3042cdd248c26d258783a1fc9d8547512
Folyd <lyshuhow@gmail.com>
2021-02-06 23:40:55 +0800

    Move common deploy docs script to core submodule

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -1,37 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-BRANCH="now"
-
-build() {
-  echo "Starting building..."
-
-  cd docs
-  sudo snap install --edge zola
-  zola build
-  mv public /tmp/public
-  cd ..
-  RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml books -d /tmp/public/index/books.js
-  RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml lints -d /tmp/public/index/lints.js
-  RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml labels -d /tmp/public/index/labels.js
-  RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml caniuse -r /tmp/caniuse -d /tmp/public/index/caniuse.js
-}
-
-deploy() {
-  echo "Starting deploying..."
-  git config --global url."https://".insteadOf git://
-  git config --global url."https://github.com/".insteadOf git@github.com:
-
-  git checkout ${BRANCH}
-  cp -vr /tmp/public/* .
-  git config user.name "GitHub Actions"
-  git config user.email "github-actions-bot@users.noreply.github.com"
-  git add .
-  git commit -m "Deploy new version docs"
-  git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" ${BRANCH}
-
-  echo "Deploy complete"
-}
-
+source ./core/deploy-docs.sh
 build
+
+RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml books -d /tmp/public/index/books.js
+RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml lints -d /tmp/public/index/lints.js
+RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml labels -d /tmp/public/index/labels.js
+RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml caniuse -r /tmp/caniuse -d /tmp/public/index/caniuse.js
+
 deploy
