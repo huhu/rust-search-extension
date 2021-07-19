8659aafe81145564b430894e675ee11043addc8c
Folyd <lyshuhow@gmail.com>
2021-07-20 00:39:30 +0800

    Remove api directory

--- api/statistics.ts
+++
@@ -1,6 +0,0 @@
-import {VercelRequest, VercelResponse} from '@vercel/node'
-
-export default (request: VercelRequest, response: VercelResponse) => {
-    const {name = 'World'} = request.query
-    response.status(200).send(`Hello ${name}!`)
-}
