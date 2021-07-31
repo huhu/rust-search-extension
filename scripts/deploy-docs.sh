64a6158a72985a2dcd62acbdc68a86cacdf275a1
Folyd <lyshuhow@gmail.com>
2021-07-31 11:44:58 +0800

    Update commands.js

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -4,9 +4,13 @@ set -e
 source ./core/deploy-docs.sh
 build
 
+# Create dir in advance
+mkdir -p /tmp/public/index
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml books -d /tmp/public/index/books.js
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml lints -d /tmp/public/index/lints.js
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml labels -d /tmp/public/index/labels.js
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml caniuse -r /tmp/caniuse -d /tmp/public/index/caniuse.js
+# Copy commands.js
+cp extension/index/commands.js /tmp/public/index/commands.js
 
 deploy
