94f2552ddbbaa71a9914c6943d74835f06db85f9
shwin <50405001+shwin0901@users.noreply.github.com>
2022-05-12 22:17:08 +0800

    Setup eslint (#153)

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+      "no-undef": "off",
+      "no-unused-vars": "off"
+    }
+}
