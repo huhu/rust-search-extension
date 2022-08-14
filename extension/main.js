3b362068e14e0d7e973825f714c1884941571ea6
Folyd <lyshuhow@gmail.com>
2022-08-14 12:05:38 +0800

    Remove uninstall url. Close #188

--- extension/main.js
+++ extension/main.js
@@ -608,7 +608,3 @@ chromeAction.onClicked.addListener(() => {
     let managePage = chrome.runtime.getURL("manage/index.html");
     chrome.tabs.create({url: managePage});
 });
-const fileNewIssue = "title=Have you found a bug? Did you feel something was missing?&body=Whatever it was, we'd love to hear from you.";
-chrome.runtime.setUninstallURL(
-    `https://github.com/huhu/rust-search-extension/issues/new?${encodeURI(fileNewIssue)}`
-);
