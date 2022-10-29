a4003885ef39df35a2ce81f522534d6e9ba34593
Folyd <lyshuhow@gmail.com>
2022-10-30 00:16:58 +0800

    Fix index build CI

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -10,10 +10,10 @@ mkdir -p /tmp/public/index
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml books -d /tmp/public/index/books.js
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml lints -d /tmp/public/index/lints.js
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml labels -d /tmp/public/index/labels.js
+RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml rustc -d /tmp/public/index/rustc.js
+RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml targets -d /tmp/public/index/targets.js
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml caniuse -r /tmp/caniuse -d /tmp/public/index/caniuse.js
 RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml rfcs -r /tmp/rfcs -d /tmp/public/index/rfcs.js
-RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml rustc -r /tmp/rfcs -d /tmp/public/index/rustc.js
-RUST_BACKTRACE=full cargo run --target-dir /tmp --manifest-path=rust/Cargo.toml targets -r /tmp/rfcs -d /tmp/public/index/targets.js
 # Copy commands.js
 cp extension/index/commands.js /tmp/public/index/commands.js
 
