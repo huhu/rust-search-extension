8e52284c50717b5289aa7a674bda3387309c301f
Folyd <lyshuhow@gmail.com>
2024-06-28 22:23:55 -0700

    Move rust tasks to query-rs submodule

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,8 +4,6 @@ use argh::FromArgs;
 
 use crate::tasks::*;
 
-mod frequency;
-mod minify;
 mod tasks;
 
 /// Options
@@ -21,14 +19,6 @@ struct Options {
 #[non_exhaustive]
 enum Subcommand {
     Advisory(AdvisoryTask),
-    Crates(CratesTask),
-    Books(BooksTask),
-    Caniuse(CaniuseTask),
-    Lints(LintsTask),
-    Labels(LabelsTask),
-    Rfcs(RfcsTask),
-    Rustc(RustcTask),
-    Targets(TargetsTask),
 }
 
 pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
@@ -37,14 +27,6 @@ fn main() -> Result<()> {
     let options: Options = argh::from_env();
     match options.subcommand {
         Subcommand::Advisory(cmd) => cmd.execute()?,
-        Subcommand::Crates(cmd) => cmd.execute()?,
-        Subcommand::Books(cmd) => cmd.execute()?,
-        Subcommand::Caniuse(cmd) => cmd.execute()?,
-        Subcommand::Lints(cmd) => cmd.execute()?,
-        Subcommand::Labels(cmd) => cmd.execute()?,
-        Subcommand::Rfcs(cmd) => cmd.execute()?,
-        Subcommand::Rustc(cmd) => cmd.execute()?,
-        Subcommand::Targets(cmd) => cmd.execute()?,
     }
     Ok(())
 }
--- rust/src/tasks/mod.rs
+++ rust/src/tasks/mod.rs
@@ -1,22 +1,6 @@
 pub use advisory::AdvisoryTask;
-pub use books::BooksTask;
-pub use caniuse::CaniuseTask;
-pub use crates::CratesTask;
-pub use labels::LabelsTask;
-pub use lints::LintsTask;
-pub use rfcs::RfcsTask;
-pub use rustc::RustcTask;
-pub use targets::TargetsTask;
 
 mod advisory;
-mod books;
-mod caniuse;
-mod crates;
-mod labels;
-mod lints;
-mod rfcs;
-mod rustc;
-mod targets;
 
 pub trait Task {
     fn execute(&self) -> crate::Result<()>;
