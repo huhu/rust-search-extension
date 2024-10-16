1cc6a4e1bdcecba1f5d677d105ee66cdd151c475
Folyd <lyshuhow@gmail.com>
2024-10-16 11:52:51 +0800

    Fix check crate svg icon

--- extension/script/docs-rs-check-crate.js
+++ extension/script/docs-rs-check-crate.js
@@ -11,11 +11,11 @@
         let svgIcon = document.querySelector(".add-to-extension .fa-svg");
         if (svgIcon) {
             svgIcon.style.color = "#666";
-            svgIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg>';
+            svgIcon.className = "fa fa-solid fa-svg fa-ban";
         }
         let content = document.querySelector(".add-to-extension-content");
         if (content) {
-            content.innerHTML = `<p>This crate version is not supported by Rust Search Extension. <br><br>After Rust Search Extension v2.0 has been released, we only support crate builded after 2024-04-20.</p>`;
+            content.innerHTML = "<p>This crate version is not supported by Rust Search Extension. <br><br>As of Rust Search Extension v2.0, we only support crates built after 2024-04-20.</p>";
         }
         console.log("No searchState.descShards found, cannot add this crate to Rust Search Extension.");
     }
