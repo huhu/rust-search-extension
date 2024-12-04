7e450169a5bd7c4680d4e80a676a6347afb403d9
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2024-12-04 15:46:33 -0500

    Always run initialization code on browser start
    This seems to work more consistently than the existing code in Firefox.
    Particularly, I could generally reproduce the extension not working if I quit
    and restart Firefox Dev multiple times.
    
    With this change, the extension loads on every browser startup, which
    seems like the desired behavior, and also should be reinitialized on
    upgrades.

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -50,9 +50,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     return true;
 });
 
-(async () => {
+async function init() {
     const defaultSuggestion = `Search std <match>docs</match>, external <match>docs</match> (~,@), <match>crates</match> (!), <match>attributes</match> (#), <match>books</match> (%), clippy <match>lints</match> (>), and <match>error codes</match>, etc in your address bar instantly!`;
     const omnibox = Omnibox.extension({ defaultSuggestion, maxSuggestionSize: Compat.omniboxPageSize() });
     await start(omnibox);
     await checkAutoUpdate();
-})();
+}
+
+chrome.runtime.onStartup.addListener(init);
+chrome.runtime.onInstalled.addListener(init);
